b1ff8d50fe58fe112cd89cd153835b5dcde1b179
Angie Rodriguez <angrodri902@gmail.com>
2018-06-12 21:57:05 -0700

    Assignment React Jams RouterCompleted.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -11,13 +12,14 @@ class App extends Component {
         <header>
           <nav>
             <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+            <Link to='/library'> Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
