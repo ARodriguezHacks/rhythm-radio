cee723d5cbabb7d19b315f4a32ceef7f7cc2f1be
Angie Rodriguez <angrodri902@gmail.com>
2018-06-25 13:27:59 -0700

    Assignment reactjams audio completed.

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,15 +10,46 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false,
+      currentlyHovered: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
     return (
       <section className="album">
-        <section is="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -33,8 +64,17 @@ class Album extends Component {
           </colgroup>
           <tbody>
           {this.state.album.songs.map( (song, index) =>
-            <tr key={index}>
-              <td>{index + 1}</td>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
+            onMouseEnter={() => this.setState({currentlyHovered: index+1})}
+            onMouseLeave={() => this.setState({currentlyHovered: false})} >
+              <td>
+                {(this.state.currentSong.title === song.title) ?
+                  <span className={this.state.isPlaying ? "ion-pause" : "ion-play"}></span> :
+                  (this.state.currentlyHovered === index+1) ?
+                  <span className="ion-play"></span> :
+                  <span className="song-number">{index+1}</span>
+                }
+               </td>
               <td>{song.title}</td>
               <td>{song.duration}</td>
             </tr>
